604fc0113787b6b15e57b6340d972df9ad63b0ea
aki-yosi <80374501+aki-yosi@users.noreply.github.com>
2021-05-24 10:55:34 +0900

    修正

---
+++ app/models/memotweet.rb
@@ -0,0 +1,2 @@
+class Memotweet < ApplicationRecord
+end
---
+++ db/migrate/20210523022945_create_memotweets.rb
@@ -0,0 +1,8 @@
+class CreateMemotweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :memotweets do |t|
+      t.string :text
+      t.timestamps
+    end
+  end
+end
